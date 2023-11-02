c4dc7c32e2e2e21ce0026091b6bb8adee0ce3929
Manh Nguyen <manh.nguyen@pfizer.com>
2023-11-02 10:58:00 +0700

    First commit

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import { appKeywords } from "@/utils/keywords";
+import { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+
+const mainFont = Montserrat({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "CINEMA",
+  description:
+    "Discover an extensive collection of movies, TV shows, and fascinating personalities. Explore our vast library now and experience the magic of cinema like never before.",
+  keywords: appKeywords,
+};
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="en">
+      <body
+        className={`${mainFont.className} mx-auto max-w-screen-2xl bg-slate-900 text-white`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
---
+++ next.config.js
@@ -0,0 +1,24 @@
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
